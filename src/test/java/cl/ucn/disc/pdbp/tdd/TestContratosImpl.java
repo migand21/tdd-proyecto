dcc0d1fbb6133b53ad5f8c740702248f72eb3703
migand21 <migand21@gmail.com>
2020-05-31 17:29:58 -0400

    test(Contratos): testing all the contratos

--- src/test/java/cl/ucn/disc/pdbp/tdd/TestContratosImpl.java
+++ src/test/java/cl/ucn/disc/pdbp/tdd/TestContratosImpl.java
@@ -24,11 +24,18 @@
 
 package cl.ucn.disc.pdbp.tdd;
 
+import cl.ucn.disc.pdbp.tdd.model.main.Ficha;
 import cl.ucn.disc.pdbp.tdd.model.main.Persona;
+import cl.ucn.disc.pdbp.tdd.model.main.Sexo;
+import cl.ucn.disc.pdbp.tdd.model.main.Tipo;
+import cl.ucn.disc.pdbp.tdd.model.utils.Entity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.ZonedDateTime;
+import java.util.List;
 
 /**
  * Class for testing Contratos
@@ -43,25 +50,42 @@ public final class TestContratosImpl {
   private static final Logger log = LoggerFactory.getLogger(TestContratosImpl.class);
 
   /**
-   * Testing the contrato 01
+   * Testing the contrato 01 : insertar ficha(paciente)
    */
   @Test
   public void testContrato1(){
 
     ContratosImpl testContratos = new ContratosImpl("jdbc:h2:mem:account");
 
+    //Creating the duenio
+    Persona duenio = new Persona("Andrea", "Contreras", "187910366", "michimalongo 1826",
+      2244397, 63887303, "asd123@gmail.com");
+    testContratos.registrarPersona(duenio);
 
+    //Creating the ficha
+    Ficha ficha = new Ficha(123456L,"ita","felino", ZonedDateTime.now(),"gato siames",
+      Sexo.HEMBRA,"blanco", Tipo.INTERNO,duenio);
+
+    //Inserting a null ficha
+    Assertions.assertThrows(IllegalArgumentException.class, () -> testContratos.registrarPaciente(null));
+
+    //Inserting a ficha
+    testContratos.registrarPaciente(ficha);
+
+    //Inserting the same ficha
+    Assertions.assertThrows(RuntimeException.class, () -> testContratos.registrarPaciente(ficha));
 
   }
 
   /**
-   * Testing the contrato 02
+   * Testing the contrato 02 : insertar duenio(persona)
    */
   @Test
   public void testContrato2(){
 
     ContratosImpl testContratos = new ContratosImpl("jdbc:h2:mem:account");
 
+    //Creating a person instance
     Persona persona1 = new Persona("Andrea", "Contreras", "152532873", "michimalongo 1826",
       2244397, 63887303, "asd123@gmail.com");
 
@@ -77,12 +101,36 @@ public final class TestContratosImpl {
   }
 
   /**
-   * Testing the contrato 03
+   * Testing the contrato 03 : obtener fichas con filtro
    */
   @Test
   public void testContrato3(){
 
     ContratosImpl testContratos = new ContratosImpl("jdbc:h2:mem:account");
+
+    //Creating the duenio
+    Persona duenio = new Persona("Andrea", "Contreras", "187910366", "michimalongo 1826",
+      2244397, 63887303, "asd123@gmail.com");
+    testContratos.registrarPersona(duenio);
+
+    //Creating fichas
+    Ficha ficha1 = new Ficha(123L,"ita","felino", ZonedDateTime.now(),"gato siames",
+      Sexo.HEMBRA,"blanco", Tipo.INTERNO,duenio);
+
+    Ficha ficha2 = new Ficha(212345L,"Andrea","canino", ZonedDateTime.now(),"san bernardo",
+      Sexo.HEMBRA,"cafe", Tipo.INTERNO,duenio);
+
+    //Inserting the fichas
+    testContratos.registrarPaciente(ficha1);
+    testContratos.registrarPaciente(ficha2);
+
+    //Finding fichas
+    List<Ficha> fichas = testContratos.buscarFicha("Andrea");
+
+    //Should be 2 if deleting duplicates works
+    Assertions.assertEquals(2,fichas.size(), "Fichas was null");
+
+    fichas.forEach(ficha -> log.debug("Ficha: {}.",Entity.toString(ficha)));
 
   }
 
